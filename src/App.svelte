a8647ec315d2c0bb7cb8e8bb88e119cea97076a0
bryanvanmoer <bryan.vanmoer51@hotmail.com>
2022-11-23 13:30:43 +0100

    add project component

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,7 @@
 <script>
+  import AddProject from "./components/AddProject.svelte";
   import Navbar from "./components/Navbar.svelte";
   import Projets from "./components/Projets.svelte";
-  import { store, addProject, deleteProject } from "./stores/store";
-
-  let projectName = "";
-  let todoName = "";
-
-  function getNewTodo(todoName) {
-    return {
-      id: `_${Math.random().toString(35).substr(2, 9)}`,
-      title: todoName,
-      complete: false,
-    };
-  }
-
-  function deleteTodo() {
-    // let cards = $store[0].cards
-    if ($store.length > 1) {
-      $store[0].todos = $store[0].todos.slice(0, -1);
-    }
-  }
-
-  function addTodo(idx, todoName) {
-    if (todoName == "") {
-      return;
-    }
-    $store[idx].todos = [getNewTodo(todoName), ...$store[idx].todos];
-  }
-
-  function updateTodo(idx, todoIdx, oldValue) {
-    $store[idx].todos[todoIdx].complete = !oldValue;
-  }
 </script>
 
 <Navbar />
@@ -38,39 +9,11 @@
 <section class="p-3">
   <div class="container g-4">
     <div class="row ">
-      <form on:submit={() => addProject(projectName)}>
-        <div class="col-4 mt-3">
-          <!-- Input to add a new project -->
-          <div class="input-group mb-3">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Project Name"
-              aria-label="Project Name"
-              aria-describedby="basic-addon2"
-              bind:value={projectName}
-            />
-            <div class="input-group-append">
-              <button
-                type="button"
-                class="btn btn-primary"
-                on:click={() => addProject(projectName)}
-              >
-                Add Project
-              </button>
-            </div>
-          </div>
-        </div>
-      </form>
+      <AddProject />
     </div>
-
     <Projets />
   </div>
 </section>
 
-<!-- Project Cards -->
 <style>
-  .checkbox {
-    padding-left: 20px;
-  }
 </style>
---
+++ src/components/AddProject.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { store, addProject, deleteProject } from "../stores/store";
+  let projectName = "";
+</script>
+
+<form on:submit={() => addProject(projectName)}>
+  <div class="col-4 mt-3">
+    <!-- Input to add a new project -->
+    <div class="input-group mb-3">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="New Project"
+        aria-label="New Project"
+        aria-describedby="basic-addon2"
+        bind:value={projectName}
+      />
+      <div class="input-group-append">
+        <button
+          type="button"
+          class="btn btn-primary"
+          on:click={() => addProject(projectName)}
+        >
+          Add Project
+        </button>
+      </div>
+    </div>
+  </div>
+</form>
